5c62b4cc2a01cee38e071840aca0081f447844e9
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2025-02-02 13:30:46 -0500

    setting up main class and methods for Menu to be instantiated

--- JS/main.js
+++ JS/main.js
@@ -1,106 +1,107 @@
 class Employee {
-  constructor(n,a) {
-    this.name = n;
-    this.age = a;
+  constructor(name,age) {
+    this.name = name;
+    this.age = age;
     this.annualSalary = 0;
-    console.log("employee created")
-  }
-  //no methods only properties
 
+  }
 }
-
 class Manager extends Employee {
-  constructor(n,a,m) {
-    super(n,a);
-    this.payRate = m;
+  constructor(name,age,pay) {
+    super(name,age);
+    this.payRate = pay;
     this.employeeType = "Manager";
+    
   }
-
   calculatePay() {
     this.annualSalary = ((this.payRate * 40 )* 52)- 1000;
   }
 }
 
 class PartTime extends Employee {
-  constructor(n,a,m,) {
-    super(n,a);
-    this.payRate = m;
-    if(this.hours < 40){
-       employeeType = "Part-Time";
-    } else {
-      employeeType = "Manager"
-    }
+  constructor(name,age,pay) {
+    super(name,age);
+    this.payRate = pay;
+    this.hours = 0;
+    this.employeeType = "Part Time"
   }
   calculatePay() {
     this.annualSalary = ((this.payRate * 40 )* 52);
   }
 }
 
-// class Menu {
-//   constructor() {
-//     this.employees = []
-//   }
-//   work(){
-//   console.log(this.name+ "is an employee ")}
-//   left(){
-//   console.log(this.name+ "is no longer an employee ")}
-
-//   displayMenu() {
-//     let options = Number(
-//       prompt(
-//         "Menu:\n1. Add Employee\n2. Remove Employee\n3. Edit Employee\n4. Display Employees\n5"
-//       )
-//     );
-//     switch (options) {
-//       case 1:
-//         choice = "Add Employee";
-//         break;
-//       case 2:
-//         choice = "Remove Employee";
-//         break;
-//       case 3:
-//         choice = "Edit Employee";
-//         break;
-//       case 4:
-//         choice = "Display Employee";
-//     }
+class Menu {
+  constructor() {
    
-//   }
-//   addEmployee() {
-//     let newEmployee = prompt(
-//       "Enter Employee's Info with commas: Name, Age, Work hours and Pay Rate"
-//     );
-//     const wordsArray = newEmployee.split(" ");
-//     alert(newEmployee+ "Employee has been added");
-//   }
-
-//   removeEmployee() {
-//     let deleteEmployee = prompt("Enter the employee's ID or name that is to be removed");
-//     const leftEmployee = deleteEmployee.filter(Employee === n || "ID");
-//     //console.log(leftEmployee)
-    
-//   }
-
-//   editEmployee() {}
-
-
-
-//   displayEmployee() {
-    //let employeesArray = [];
-//   }
+  console.log("I have the greatest TEAM!")
+  console.log("ID\tName\tSalary\tHours\tPay\tFT/PT")
+  }
   
-// }
+  displayMenu() {
+    let options = Number(
+      prompt(
+        "Menu:\n1. Add Employee\n2. Remove Employee\n3. Edit Employee\n4. Display Employees\n5"
+      )
+    );
+      switch (options) {
+        case 1:"Add Employee";
+          addEmployee();
+          break;
+        case 2:
+          "Remove Employee";
+          removeEmployee();
+          break;
+        case 3:
+          "Edit Employee Pay";
+          editEmployee();
+          break;
+        case 4:
+          "Display Employee";
+          displayEmployee();
+          break
+      }
+   
+  }
+  addEmployee(name,age) {
+    let newEmployee = prompt(
+      "Enter Employee's Info with commas: Name, Age, Work hours and Pay Rate"
+    );
+    if(this.hours >= 40){
+      newEmployee = new Manager(name,age,pay)
+    } else {
+      newEmployee = new PartTime(name, age, pay,hours)
+    }
+
+    if(newEmployee !== null){
+        alert(name+ "is an added employee")
+      }
+      else{
+        alert("Try again, employee information needed")
+      }
+    let newArray = newEmployee.split(",")
+     
+  }
+  removeEmployee(name) {
+    let deleteEmployee = prompt("Enter employee's ID or name to be removed");
+    let leftArray = [];
+    for(let i =leftArray.length; 0<i;)
+    //const leftEmployee = deleteEmployee.filter(Employee === n);
+    console.clear()
+    left()
+    alert(name+ "is no longer with the company")
+  }
+
+  editEmployee(name) {
+    let change = prompt("Type employee's ID for changes")
+    
+  }
+
+  displayEmployee(name, age, annualSalary, hours, pay) {
+    
+  }
+  
+}
 
 ((e) => {
-  //const menu = new Menu();
-  const employee1 = new Employee
-  employee1.name = "Sam"
-  employee1.age = 24;
-  employee1.hours = 40;
-  employee1.payRate = 5;
-  console.log()
-  console.log(employee1.name, employee1.age, employee1.annualSalary, employee1.hours, employee1.payRate, employee1.employeeType)
-  
-
-  
+  const menu = new Menu();
 })();
