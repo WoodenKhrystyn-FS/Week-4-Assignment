fcffc41fe2e6e4ae51fe79002b1e548369e7ec29
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2025-02-02 16:41:07 -0500

    Working on methods within Main class

--- JS/main.js
+++ JS/main.js
@@ -39,12 +39,11 @@ class Menu {
     this.payRate = pay;
     this.employeeType = " ";
     console.log("I have the greatest TEAM!");
-    console.log("ID\tName\tSalary\tHrs\tPay\tFT/PT");
     const oneEmployee = new Employee();
     oneEmployee.name = "Samuel";
-    oneEmployee.annualSalary = 9660;
+    oneEmployee.annualSalary = 9400;
     oneEmployee.age = 32;
-    oneEmployee.hours = 41;
+    oneEmployee.hours = 40;
     oneEmployee.pay = 5;
     oneEmployee.employeeType = "Manager";
     console.log(
@@ -62,10 +61,10 @@ class Menu {
     );
     const twoEmployee = new Employee();
     twoEmployee.name = "Jimbo";
-    twoEmployee.annualSalary = 1664;
+    twoEmployee.annualSalary = 4992;
     twoEmployee.age = 23;
-    twoEmployee.hours = 32;
-    twoEmployee.pay = 1;
+    twoEmployee.hours = 12;
+    twoEmployee.pay = 8;
     twoEmployee.employeeType = "Part Time";
     console.log(
       "2" +
@@ -133,43 +132,33 @@ class Menu {
     let newEmployee = prompt(
       "Enter Employee's Info with commas: Name, Age, Work hours and Pay Rate"
     );
-    if (newEmployee !== null) {
-      alert(name + "is an added employee");
-    } else {
-      alert("Try again, employee information needed");
-    }
-
     if (this.hours >= 40) {
       newEmployee = new Manager(name, age, pay);
     } else {
       newEmployee = new PartTime(name, age, pay, hours);
     }
-    let workerArray = newEmployee.split(",");
+
+    let newArray = workerArray.split(",");
+    console.log(newArray)
   }
-  
-  
   removeEmployee(name) {
     let deleteEmployee = prompt("Enter employee's ID or name to be removed");
-    let leftArray = [];
-    for (let i = leftArray.length; 0 < i; )
-      //const leftEmployee = deleteEmployee.filter(Employee === n);
-      console.clear();
-    left();
+    //for (let i = leftArray.length; 0 < i; )
+    let leftEmployee = deleteEmployee.filter(Employee === n);
     alert(name + "is no longer with the company");
+    let leftArray = [];
+    console.clear();
   }
 
-  editEmployee(name) {
+  editEmployee() {
     let change = prompt("Type employee's ID for changes");
   }
 
-  displayEmployee(name, age, annualSalary, hours, pay, employeeType) {
-    this.name = name;
-    this.age = age;
-    this.annualSalary = 0;
-    this.hours = 0;
-    this.payRate = pay;
-    this.employeeType = "";
+  displayEmployee() {
+    console.log("The Wooden Table")
+    console.log("ID\tName\tSalary\tHrs\tPay\tFT/PT");
   }
+  
 }
 
 ((e) => {
